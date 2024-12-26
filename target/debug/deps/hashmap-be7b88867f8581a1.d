b0acff1ac876aae127eb1007b563c3176deba249
grainme <marouanebouf3@gmail.com>
2024-12-27 00:49:34 +0100

    more methods, test

---
+++ target/debug/deps/hashmap-be7b88867f8581a1.d
@@ -0,0 +1,9 @@
+/home/grainme/codespace/rusty_hashmap/target/debug/deps/libhashmap-be7b88867f8581a1.rmeta: src/lib.rs Cargo.toml
+
+/home/grainme/codespace/rusty_hashmap/target/debug/deps/hashmap-be7b88867f8581a1.d: src/lib.rs Cargo.toml
+
+src/lib.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
