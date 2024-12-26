3e6ba811e77028e57b96bde226d4b5448035b943
grainme <marouanebouf3@gmail.com>
2024-12-27 00:47:36 +0100

    more methods, test

--- target/debug/deps/hashmap-abdf5611c4b500a0.d
+++
@@ -1,5 +0,0 @@
-/home/grainme/codespace/rusty_hashmap/target/debug/deps/libhashmap-abdf5611c4b500a0.rmeta: src/lib.rs
-
-/home/grainme/codespace/rusty_hashmap/target/debug/deps/hashmap-abdf5611c4b500a0.d: src/lib.rs
-
-src/lib.rs:
