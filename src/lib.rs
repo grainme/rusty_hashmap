c7af28741dff378329a2f9a96e096cda2e7489a5
grainme <marouanebouf3@gmail.com>
2024-12-27 16:58:40 +0100

    iterator support, test

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,55 @@ use std::{hash::{DefaultHasher, Hash, Hasher}, mem};
 const INITIAL_NBUCKET: usize = 1;
 
 pub struct Hashmap<K, V> {
-    bucket: Vec<Vec<(K, V)>>,
+    buckets: Vec<Vec<(K, V)>>,
     items: usize,
+}
+
+pub struct IterHashmap<'a, K, V> {
+    map: &'a Hashmap<K, V>,
+    current_bucket: usize,
+    current_item: usize,
+}
+    
+impl<'a, K, V> Iterator for IterHashmap<'a, K, V> {
+    type Item = (&'a K, &'a V);
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            match self.map.buckets.get(self.current_bucket) {
+                Some(bucket) => {
+                    match bucket.get(self.current_item) {
+                        Some((ref key, ref val)) => {
+                            break Some((key, val))
+                        }
+                        None => {
+                            continue;
+                        }
+                    }
+                }
+                None => break None,
+            }
+        }
+    } 
+}
+
+
+impl<'a, K, V> IntoIterator for &'a Hashmap<K, V> {
+    type IntoIter = IterHashmap<'a, K, V>;
+    type Item = (&'a K, &'a V);
+
+    fn into_iter(self) -> Self::IntoIter {
+        IterHashmap {
+            map: self,
+            current_bucket: 0,
+            current_item: 0,
+        }
+    }
 }
 
 impl<K, V> Hashmap<K, V> {
     pub fn new() -> Self {
         Hashmap { 
-            bucket: Vec::new(),
+            buckets: Vec::new(),
             items: 0,
         }
     }
@@ -21,7 +62,9 @@ impl<K, V> Default for Hashmap<K, V> {
      fn default() -> Self {
          Self::new()
      }
-} 
+}
+
+
 
 impl<K, V> Hashmap<K, V>
 where K: Eq + Hash 
@@ -29,16 +72,16 @@ where K: Eq + Hash
     fn bucket(&self, key: &K) -> usize {
         let mut hasher = DefaultHasher::new();
         key.hash(&mut hasher);
-        (hasher.finish() & (self.bucket.len() - 1) as u64) as usize
+        (hasher.finish() & (self.buckets.len() - 1) as u64) as usize
     }
     
     pub fn insert(&mut self, key: K, value: V) -> Option<V> {
-        if self.bucket.is_empty() || self.items > 3 * self.bucket.len() / 4 {
+        if self.buckets.is_empty() || self.items > 3 * self.buckets.len() / 4 {
             self.resize();
         }
 
         let bucket = self.bucket(&key); 
-        let bucket = &mut self.bucket[bucket];
+        let bucket = &mut self.buckets[bucket];
         
         // ref, we don't wanna take ownership (otherwise we'll break the data structure)
         for &mut (ref ekey, ref mut eval) in bucket.iter_mut() {
@@ -52,7 +95,7 @@ where K: Eq + Hash
     } 
 
     pub fn resize(&mut self){
-        let target_size = match self.bucket.len() {
+        let target_size = match self.buckets.len() {
             0 => INITIAL_NBUCKET,
             n => 2 * n,
         };
@@ -60,17 +103,17 @@ where K: Eq + Hash
         let mut new_bucket = Vec::with_capacity(target_size);
         new_bucket.extend((0..target_size).map(|_| Vec::new()));
          
-        for (key, value) in self.bucket.iter_mut().flat_map(|bucket| bucket.drain(..)) {
+        for (key, value) in self.buckets.iter_mut().flat_map(|bucket| bucket.drain(..)) {
             let mut hasher = DefaultHasher::new();
             key.hash(&mut hasher);
             let bucket: usize = (hasher.finish() & (new_bucket.len() - 1) as u64) as usize;
             new_bucket[bucket].push((key, value));
         }
-        self.bucket = new_bucket;
+        self.buckets = new_bucket;
     }
 
     pub fn get(&self, key: &K) -> Option<&V> {
-        self.bucket[self.bucket(key)]
+        self.buckets[self.bucket(key)]
             .iter()
             .find(|(ref ekey, _)| ekey == key)
             .map(|(_, ref eval)| eval)
@@ -78,14 +121,14 @@ where K: Eq + Hash
 
 
     pub fn contains_key(&self, key: &K) -> bool {
-        self.bucket[self.bucket(key)]
+        self.buckets[self.bucket(key)]
             .iter()
             .any(|(ref ekey, _)| ekey == key)
     }
 
     pub fn remove(&mut self, key: K) -> Option<V> {
         let bucket = self.bucket(&key);
-        let bucket = &mut self.bucket[bucket];
+        let bucket = &mut self.buckets[bucket];
         // ? works with both result and option (sugar)
         let ind: usize = bucket.iter().position(|(ref ekey, _)| ekey == &key)?;
         self.items -= 1;
---
+++ target/debug/examples/std_1.d
@@ -0,0 +1,1 @@
+/home/grainme/codespace/rusty_hashmap/target/debug/examples/std_1: /home/grainme/codespace/rusty_hashmap/examples/std_1.rs /home/grainme/codespace/rusty_hashmap/src/lib.rs
